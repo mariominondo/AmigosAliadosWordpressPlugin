c8ca5a099cb54bc87393c3c7f1f72c719ed4338a
Minondo Pineda <Mario.Minondo@bda.local>
2021-10-21 11:09:22 -0600

    Agregar link al menu del Control Panel del Admin

---
+++ AmigosAliadosWordpressPlugin/AmigosAliadosWordpressPlugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Amigos Aliados Wordpress Plugin
+Description: Agrega información desde un archivo CSV y lo muestra por medio de shortcodes
+Author: Mario Minondo
+*/
+
+require_once plugin_dir_path(__FILE__) . 'includes/aawp-functions.php';
