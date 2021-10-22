d28cad9027ede12d16e03355c56674945cb89d34
Minondo Pineda <Mario.Minondo@bda.local>
2021-10-22 14:37:56 -0600

    mm-farmacias.html

---
+++ AmigosAliadosWordpressPlugin/includes/aawp-first-admin-control-panel-page.php
@@ -0,0 +1,4 @@
+<div class="wrap">
+    <h1>Hey!</h1>
+    <p>Plugin para administrar los Amigos Aliados</p>
+</div>
--- AmigosAliadosWordpressPlugin/includes/aawp-functions.php
+++ AmigosAliadosWordpressPlugin/includes/aawp-functions.php
@@ -7,12 +7,12 @@
 add_action('admin_menu', 'aawp_Agregar_Admin_Link()');
 // Agrega un nuevo top level menu link al Control Panel del Admin
 function aawp_Agregar_Admin_Link() {
-    if( function_exists('add_menu_page'){
+    //if( function_exists('add_menu_page'){
         add_menu_page (
             'Amigos Aliados', // Título de la página
             'Amigos Aliados', // Texto a mostrar en el link del menú
             'manage_options', // Capacidad requerida para mostrar el link
             'includes/aawp-first-admin-control-panel-page.php' // El 'slug' - archivo para mostrar cuando se da click al link
         );
-    }
+    //}
 }
